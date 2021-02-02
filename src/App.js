bad8ced3094acb1e2f749c8728425b0cdddd09dc
bilal07 <mouadine.bilal@gmail.com>
2021-02-02 10:50:15 +0100

    crete app without redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    count : 0
+  }
+  increase = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+  decrease = () => {
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
   render(){
     return (
       <div className="App">
-        Install Redux
+        <button onClick={this.increase}> + </button>
+        <div>{this.state.count}</div>
+        <button onClick={this.decrease}> - </button>
       </div>
     );
   }
