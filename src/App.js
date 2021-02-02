9533c81edc4dacf5e5e857b8f3826d993b98728e
bilal07 <mouadine.bilal@gmail.com>
2021-02-02 14:10:54 +0100

    mapStateToProps function

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 class App extends Component {
-  state = {
-    count : 0
-  }
+
   increase = () => {
     this.setState({
-      count: this.state.count + 1
+      count: this.props + 1
     })
   }
   decrease = () => {
     this.setState({
-      count: this.state.count - 1
+      count: this.props - 1
     })
   }
   render(){
+    console.log(this.props)
     return (
       <div className="App">
         <button onClick={this.increase}> + </button>
-        <div>{this.state.count}</div>
+        <div>{this.props.count}</div>
         <button onClick={this.decrease}> - </button>
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    count : state.count
+  }
+}
+export default connect(mapStateToProps)(App);
